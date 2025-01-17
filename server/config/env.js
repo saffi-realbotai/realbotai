3d07d904dd243a72564f761ce4135da3899f0d80
unknown <saffi@realbotai.com>
2025-01-17 16:38:03 +0500

    Remove all files from repository but keep them locally

--- server/config/env.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config(); // Load .env file
-
-module.exports = {
-  MONGO_URI: process.env.MONGO_URI,
-  PORT: process.env.PORT || 3000,
-  AI_SECRET_KEY: process.env.AI_SECRET_KEY // Add any secret key you need
-};
--- server/middlewares/errorMiddleware.js
+++
@@ -1,8 +0,0 @@
-// Global error handler middleware
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: err.message });
-  };
-  
-  module.exports = errorHandler;
-  
