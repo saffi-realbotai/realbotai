aba30472c46cbfde17b1b167a20361f014344889
Suraksha <surakshak571@gmail.com>
2025-01-24 01:50:46 +0500

    header footer

--- client/src/landingPage/components/Header.js
+++ client/src/landingPage/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Header.css";
-import logo from "./assets/images/logo.svg"; // Replace with your actual logo
+import logo from "./assets/images/whitelogo.png"; // Replace with your actual logo
 
 const Header = () => {
   return (
     <header className="navbar">
       <div className="navbar-left">
-        <img src={logo} alt="Logo" className="navbar-logo" />
+        <img src={logo} alt="Logo" className="navbar-logo" style={{width: "150px" ,height: "50px"}} />
       </div>
       <div className="navbar-right">
         <button className="book-call-btn">Book a Call</button>
